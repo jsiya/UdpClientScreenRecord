6090271b02d2049117ff499edde806e3a8c2dbc1
jsiya <jabbarovasia@gmail.com>
2023-12-06 19:21:45 +0400

    First

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        private const int bufferSize = 8000;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void RequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("192.168.4.116"), 27001);
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    // Ekranin olcusu
+                    Screen screen = Screen.PrimaryScreen;
+                    string screenSizeInfo = $"{screen.Bounds.Width},{screen.Bounds.Height}";
+
+
+                    byte[] sizeBuffer = Encoding.Default.GetBytes(screenSizeInfo);// servere her client ucun ayri ekran olcusu gedir
+                    client.SendTo(sizeBuffer, serverEndPoint);
+
+
+                    byte[] requestBuffer = Encoding.Default.GetBytes("CaptureScreen"); // screenshot ucun sorgu
+                    client.SendTo(requestBuffer, serverEndPoint);
+
+                    while (true)
+                    {
+                        byte[] imageBytes = ReceiveImage(client, serverEndPoint); //shekili tam qebul elemek ucun method
+                        using (MemoryStream ms = new MemoryStream(imageBytes))
+                        {
+                            Bitmap image = new Bitmap(ms);
+                            ImageFrame.Dispatcher.Invoke(() => ImageFrame.Source = ToBitmapImage(image)); //gelen byte array stream kimi bitmapa verdikden sonra image cevirmek ucun
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private byte[] ReceiveImage(Socket client, IPEndPoint serverEndPoint)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                while (true)
+                {
+                    byte[] dataBuffer = new byte[bufferSize];
+                    EndPoint serverResponse = new IPEndPoint(IPAddress.Any, 0);
+
+                    // her defe gelen hisse oxunub streame yazilir
+                    int bytesRead = client.ReceiveFrom(dataBuffer, ref serverResponse);
+                    ms.Write(dataBuffer, 0, bytesRead);
+
+                    if (bytesRead < bufferSize) //gelen datanin uzunlugu bufferde azdisa sonuncunu oxuyub break edir
+                        break;
+                }
+
+                return ms.ToArray();
+            }
+        }
+
+        private BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Jpeg);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        private const int BufferSize = 8000;
+
+        static void Main(string[] args)
+        {
+            var listener = new UdpClient(27001);
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("UDP server listening on port 27001");
+
+            while (true)
+            {
+                byte[] sizeBuffer = listener.Receive(ref remoteEndPoint);
+                string screenSizeInfo = Encoding.Default.GetString(sizeBuffer); // clientin ekran olcusunu qebul edir
+                string[] screenSize = screenSizeInfo.Split(','); //en, uzunluq
+
+                int screenWidth = int.Parse(screenSize[0]);
+                int screenHeight = int.Parse(screenSize[1]);
+
+                byte[] buffer = listener.Receive(ref remoteEndPoint);
+                string msg = Encoding.Default.GetString(buffer);
+
+                if (msg == "CaptureScreen")
+                {
+                    while (true)
+                    {
+                        using Bitmap memoryImage = new Bitmap(screenWidth, screenHeight);
+
+                        using Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+                        memoryGraphics.CopyFromScreen(0, 0, 0, 0, new Size(screenWidth, screenHeight));
+
+                        using MemoryStream ms = new MemoryStream();
+                        memoryImage.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                        SendImage(listener, remoteEndPoint, ms.ToArray()); //shekili gonderen method
+                    }
+                }
+
+                Console.WriteLine($"{remoteEndPoint} : {msg}");
+            }
+        }
+
+        static void SendImage(UdpClient listener, IPEndPoint remoteEndPoint, byte[] imageData)
+        {
+            int totalChunks = (int)Math.Ceiling((double)imageData.Length / BufferSize); //sekili gondereceyi addim sayi
+
+            for (int i = 0; i < totalChunks; i++)
+            {
+                int offset = i * BufferSize; // umumi gonderilen byte sayi
+                int chunkSize = Math.Min(BufferSize, imageData.Length - offset); //gonderilen byte sayi umumi bufferin olcusunden azdisa qalani gondermek ucun
+                byte[] bufferForSending = new byte[chunkSize];
+
+                Array.Copy(imageData, offset, bufferForSending, 0, chunkSize); //o qeder byte yeni buffere kopyalanir
+
+                listener.Send(bufferForSending, bufferForSending.Length, remoteEndPoint); // ve gonderilir
+            }
+
+            Console.WriteLine($"Image sent to {remoteEndPoint}");
+        }
+    }
+}
